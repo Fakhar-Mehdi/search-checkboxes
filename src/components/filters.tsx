24cfb0124655e9f62925dee160c5511376c4b9f7
Fakhar-Mehdi <fakharmehdi.2525@gmail.com>
2025-10-02 15:59:57 +0500

    implementation done

---
+++ src/components/filters.tsx
@@ -0,0 +1,32 @@
+import React, { FormEvent } from 'react'
+
+interface FiltersProps {
+    checkboxList: string[]
+    handleCheckbox: (e: FormEvent<HTMLInputElement>) => void
+}
+
+const Filters = ({ checkboxList, handleCheckbox }: FiltersProps) => {
+    return (
+        <div className='flex justify-center items-center gap-4 mb-5 mt-[85px] md:mt-28 lg:mt-32'>
+            {checkboxList.map((checkbox: string) => (
+                <div key={checkbox} className=''>
+                    <label
+                        htmlFor={checkbox}
+                        className='sm:text-sm md:text-xl lg:text-2xl'
+                    >
+                        <input
+                            type='checkbox'
+                            onChange={handleCheckbox}
+                            value={checkbox}
+                            className='mr-2 sm:w-3 sm:h-3 md:w-4 md:h-4 lg:w-5 lg:h-5'
+                            id={checkbox}
+                        />
+                        {checkbox.charAt(0).toUpperCase() + checkbox.slice(1)}
+                    </label>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default Filters
